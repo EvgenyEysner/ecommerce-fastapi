c1341804a381dd4841e6a96f0476851e3a517ddb
Evgeny Eysner <evgeny.eysner@gmail.com>
2023-09-19 19:43:14 +0200

    feat: add content

--- services/frontend/app/(root)/page.tsx
+++ services/frontend/app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import {Container} from "@/app/Container";
 import {HomeBanner} from "@/app/components/banner/HomeBanner";
+import {products} from "@/utils/products";
+import {truncateText} from "@/utils/truncateText";
 
 export default function Home() {
   return (
@@ -7,6 +9,15 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner/>
+        </div>
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {products.map((product: any) => {
+            return (
+              <div key={product.id}>
+                {truncateText(product.name)}
+              </div>
+            )
+          })}
         </div>
       </Container>
     </div>
