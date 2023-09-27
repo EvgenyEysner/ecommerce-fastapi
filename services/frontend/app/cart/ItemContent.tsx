be499ae49580b2bc797b769ca691563f3a2de9f5
Evgeny Eysner <evgeny.eysner@gmail.com>
2023-09-27 18:29:45 +0200

    feat: add remove function for product

--- services/frontend/app/cart/ItemContent.tsx
+++ services/frontend/app/cart/ItemContent.tsx
@@ -7,8 +7,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { SetQuantity } from '../components/product/SetQuantity';
+import { useCart } from '@/hooks/useCart';
 
 export const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
+  const { handleRemoveProductFormCart } = useCart()
+
   return (
     <div className='grid grid-cols-5 text-xs md:text-sm gap-4 border-t-[1.5px] border-slate-200 py-4 items-center'>
       <div className='col-span-2 justify-self-start flex gap-2 md:gap-4'>
@@ -24,7 +27,7 @@ export const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
           <div>{item.selectedImg.color}</div>
           <div className='w-[70px]'>
             <button className='text-slate-500 underline' onClick={() => {
-
+              handleRemoveProductFormCart(item);
             }}>
               Удалить
             </button>
